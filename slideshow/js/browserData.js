4facdac1f0ece1eea5a2865f38ac20dfaff6fe33
copperfox2020 <62559282+copperfox2020@users.noreply.github.com>
2024-04-12 17:14:10 -0400

    ok hope this works

--- slideshow/js/browserData.js
+++ slideshow/js/browserData.js
@@ -1,9 +1,15 @@
-const btn = document.getElementById('btn');
-const output = document.getElementById('container');
-const url = 'finalItems.json';
-const url1 = '../copperfoxantiques.com.working/slideshow/js/finalItems.json';
-btn.onclick = () => {
-    console.log('clicked');
-}
+//const btn = document.getElementById('btn');
+//const output = document.getElementById('container');
+const url = 'data/itemFinal4.11.24.json';
+//const url1 = '../copperfoxantiques.com.working/slideshow/js/finalItems.json';
+//btn.onclick = () => {
+    //console.log('clicked');
+//}
 //console.log(btn);
 
+fetch(url)
+.then(res => res.json())
+.then(data => {
+    const item = data;
+    console.log(item);
+});
